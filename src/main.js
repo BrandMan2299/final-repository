9f688251d2a4184df06f306a38d0c3408de997aa
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-07-19 16:26:02 +0300

    Revert "Revert "sql date converter+ button listener+ fixes""
    This reverts commit d59e872abaff2a81da80800845dc47598191408e.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 function adding(){
     const list=document.querySelector("ul");
-    const prioritySelection=querySelector("#prioritySelector");
+    const prioritySelection=document.querySelector("#prioritySelector");
     const input=document.querySelector("#textInput");
     const listItem=document.createElement("li");
     const container=document.createElement("div");
     const priority=document.createElement("span");
-    const time=document.createElement("time");
-    const text=document.createElement("p");
+    const time=document.createElement("span");
+    const text=document.createElement("span");
     container.className="todoContainer";
     priority.className="todoPriority";
     time.className="todoCreatedAt";
@@ -14,4 +14,23 @@ function adding(){
     priority.innerHTML=prioritySelection.value;
     time.innerHTML=sqlDate();
     text.innerHTML=input.value;
+    input.value="";
+    container.appendChild(priority);
+    container.appendChild(time);
+    container.appendChild(text);
+    listItem.appendChild(container);
+    list.appendChild(listItem);
 }
+function sqlDate(){
+    const d=new Date();
+    const dateArr=[d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()];
+    for(let i=0;i<dateArr.length;i++){
+        if(dateArr[i]<10){
+            dateArr[i]="0"+dateArr[i];
+        }
+    }
+    return `${d.getFullYear()}-${dateArr[0]}-${dateArr[1]} ${dateArr[2]}:${dateArr[3]}:${dateArr[4]}`;
+}
+
+const addButton=document.querySelector("#addButton");
+addButton.addEventListener("click", adding);
